14a209aa160f8320815743d6df863de1178f13fd
Sorn Piseth <pxsxth1214@gmail.com>
2026-04-18 23:19:59 +0700

    Add seeder to start script

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ php artisan view:cache
 echo "▶ Running migrations..."
 php artisan migrate --force
 
+echo "▶ Seeding database..."
+php artisan db:seed --force
+
 echo "▶ Linking storage..."
 php artisan storage:link || true
 
