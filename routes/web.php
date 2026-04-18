2dc2f09ef49511fa6e38dfea4aabc54d0b527e2a
Sorn Piseth <pxsxth1214@gmail.com>
2026-04-18 23:26:24 +0700

    Disable seeder temporarily

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/seed', function() {
+    \Artisan::call('db:seed --force');
+    return 'Seeded!';
+});
