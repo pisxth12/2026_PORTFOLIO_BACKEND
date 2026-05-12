d51ac7b552938de43ca90e9dd97c7323516ac63b
Sorn Piseth <pxsxth1214@gmail.com>
2026-05-13 01:30:54 +0700

    dataseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (!\App\Models\User::exists()) {
-            $this->call(AdminUserSeeder::class);
-        }
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('Admin123'),
+            ]
+        );
     }
 }
